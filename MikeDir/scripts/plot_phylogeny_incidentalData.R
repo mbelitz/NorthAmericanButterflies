5bd439dcffa55aa4f30613f278a0701502148d91
mbelitz <mbelitz@ufl.edu>
2022-05-22 13:05:55 -0400

    update with correct phylogeny for NABA & iNaturalist species

--- MikeDir/scripts/plot_phylogeny_incidentalData.R
+++ MikeDir/scripts/plot_phylogeny_incidentalData.R
@@ -11,12 +11,15 @@ head(t$tip.label)
 # read in inat species
 spp <- read.csv("MikeDir/data/SimpletonsSpeciesTraits.csv")
 spp <- spp %>% 
-  mutate(name = paste(Genus..iNat., species..iNat.)) %>% 
-  filter(name != " ") %>% 
-  select(name, Simpleton.grouping.code)
+  mutate(name = paste(Genus..iNat., species..iNat.))
+
+spp <- spp %>% 
+  mutate(name = if_else(condition =  name == " ", 
+                        true = paste(NABA.Genus, NABA.species),
+                        false = name))
 
 # read in tiplabels that will match Spp of mdf
-tip_labels_df <- read.csv("MikeDir/data/iScience/tipLabel.csv") # all tip labels
+tip_labels_df <- read.csv("MikeDir/data/iScience/tipLabel_incidental.csv") # all tip labels
 head(tip_labels_df$validName)
 
 t$tip.label <- tip_labels_df$validName
@@ -29,7 +32,8 @@ tip_labels_df_lj <- left_join(tip_labels_df, spp, by = c("validName" = "name")) 
 
 # great, we have traits for all species so let's make into mdf
 mdf <- tip_labels_df_lj %>% 
-  filter(Simpleton.grouping.code != "OTH")
+  filter(Simpleton.grouping.code != "OTH") %>% 
+  filter(Simpleton.grouping.code != "Oth")
 
 # drop stuff not in phylogy/analysis
 mdf_phylo <- mdf %>% 
@@ -45,14 +49,23 @@ t2 <- ape::drop.tip(t2, tip = "Lycaeides melissa")
 
 p <- ggtree(t2, layout='circular')
 
-phylo_df <- data.frame(species = t2$tip.label, node = t2$Nnode)
-t3 <- read.csv("MikeDir/data/t3.csv")
+phylo_df <- as.data.frame(p$data) 
+phylo_df <- left_join(phylo_df, tip_labels_df, by = c("label" = "validName"))
+phylo_df <- phylo_df%>% 
+  filter(!is.na(tip_label))
+library(stringr)
+phylo_df <- phylo_df %>% 
+  mutate(family = stringr::word(tip_label, 1,1,sep = fixed("_")))
+phylo_df <- left_join(mdf_phylo, phylo_df, by = "tip_label")
+
+
+
 
 p +
-  geom_tile(data = t3, aes(x = 135, y = y, fill = Simpleton.grouping.code),
+  geom_tile(data = phylo_df, aes(x = 135, y = y, fill = Simpleton.grouping.code),
             width = 20, color = "white",
             inherit.aes = F) + 
-  geom_point(data = t3, aes(x = x, y = y, color = family), size = 2) +
+  geom_point(data = phylo_df, aes(x = x, y = y, color = family), size = 2) +
   scale_fill_viridis_d(option = "mako") +
   scale_color_viridis_d(option = "inferno") +
   labs(fill = "Overwintering stage", color = "Family")
